1d2aef3908b4048a23bdfc3d10f8429c4a310baa
Edward Kim <edbeta12@gmail.com>
2014-07-21 00:15:47 -0400

    Added new prototypes for mallocing strings from text columns, checking if columns contain null values, and extracting personal profile from the database

--- ssdie.h
+++ ssdie.h
@@ -1,6 +1,10 @@
+//SQLite3 prototypes
 sqlite3_stmt* get_sql_statement(char *,sqlite3*); 
 sqlite3* open_sql_database(char *);
 void free_sql_statement(sqlite3_stmt*);
+void check_ambig_text_column_(sqlite3_stmt*, char*,int,int);
 
+//Extractor prototypes
 int extract_contacts(sqlite3*);
-
+int extract_profile(sqlite3*);
+void* alloc_sql_text_column(sqlite3_stmt*, int);
