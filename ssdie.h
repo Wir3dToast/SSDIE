48bf412055c56b0bc2b78b0a7b2da0f267997f8c
Edward Kim <edbeta12@gmail.com>
2014-07-21 23:09:48 -0400

    Removed prototype for alloc_sql_statement.

--- ssdie.h
+++ ssdie.h
@@ -2,7 +2,7 @@
 sqlite3_stmt* get_sql_statement(char *,sqlite3*); 
 sqlite3* open_sql_database(char *);
 void free_sql_statement(sqlite3_stmt*);
-void check_ambig_text_column_(sqlite3_stmt*, char*,int,int);
+void check_ambig_text_column_(sqlite3_stmt*, char*,int);
 
 //Extractor prototypes
 int extract_contacts(sqlite3*);
