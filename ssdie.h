877d8ed691d7aedec4e713710dc3e64ce901c281
Edward Kim <edbeta12@gmail.com>
2014-07-22 00:46:24 -0400

    Removed all unneeded changes

--- ssdie.h
+++
@@ -1,9 +0,0 @@
-//SQLite3 prototypes
-sqlite3_stmt* get_sql_statement(char *,sqlite3*); 
-sqlite3* open_sql_database(char *);
-void free_sql_statement(sqlite3_stmt*);
-void check_ambig_text_column_(sqlite3_stmt*, char*,int);
-
-//Extractor prototypes
-int extract_contacts(sqlite3*);
-int extract_profile(sqlite3*);
--- ssdie_depre.h
+++
@@ -1,2 +0,0 @@
-//a header file for functions on used that may be used for later
-void* alloc_sql_text_column(sqlite3_stmt*, int);
