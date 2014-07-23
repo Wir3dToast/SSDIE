d52d35a7e151ddff277d56688bafb4c3fd395fae
Edward Kim <edbeta12@gmail.com>
2014-07-23 00:01:20 -0400

    Implemented directory existence checking and creation. Also implemented concatentation of pathnames and filenames to make new pathnames for new directories and new files

--- src/file_functions.c
+++ src/file_functions.c
@@ -1,2 +1,36 @@
 #include <stdio.h>
+#include <stdlib.h>
 #include <sys/stat.h>
+#include <sys/types.h>
+#include <errno.h>
+#include <string.h>
+
+#include "header/file_functions.h"
+
+void check_make_result_dir(char* pathname) {
+    int status = mkdir(pathname, S_IRUSR | S_IWUSR | S_IXUSR);
+    
+    switch(status) {
+	case EEXIST: printf("%s","Directory of this name already exists");
+		     exit(EXIT_FAILURE);
+        case ENOENT: printf("%s","Pathname does not exist or Path is empty");
+		     exit(EXIT_FAILURE);
+        case ENOTDIR: printf("%s", "Component of the parth prefix is not a directory");
+		      exit(EXIT_FAILURE);
+    }
+}
+
+char* concat_pathname_elements(char* pathname, char* filename) {
+    size_t len_of_pathname = strlen(pathname);
+    size_t len_of_file = strlen(filename);
+
+    char* full_pathname = malloc(len_of_pathname + len_of_file + 2 * sizeof(char)); //Plus 2 for end-of-string terminator and "/"
+      
+    memcpy(full_pathname,pathname,len_of_pathname);
+    memset(full_pathname + len_of_pathname,'/',1);
+    memcpy(full_pathname + len_of_pathname + 1, filename, len_of_file + 1);
+
+    return full_pathname;
+
+}
+
---
+++ src/header/file_functions.h
@@ -0,0 +1,3 @@
+//File Function protoypes
+void check_make_result_dir(char *);
+char* concat_pathname_elements(char*,char*);
