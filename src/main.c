e616e708d090f60109edf344a88976c09eea35be
Edward Kim <edbeta12@gmail.com>
2014-07-23 16:14:10 -0400

    Moved main function to different C file

---
+++ src/main.c
@@ -0,0 +1,35 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <sqlite3.h>
+
+#include "header/ssdie.h"
+#include "header/sql_functions.h"
+#include "header/file_functions.h"
+
+
+sqlite3* db;
+char* pathname;
+
+int main(int argc, char** argv) {
+
+  
+    if(argc != 3) {
+      fprintf(stderr, "USAGE: ssdie [DB Path] [DIRPATH]");
+      return EXIT_FAILURE;
+    }
+    
+    char* dbname = argv[1];
+    pathname = argv[2]; //Rootdir the directory in which the directory that will contain the result files will be contained in.
+    
+    db = open_sql_database(dbname);
+    check_make_result_dir(pathname); //Create the directory
+
+    printf("\nExtracting to directory: %s",pathname);
+    printf("\n%s","Starting to extract Contacts"); //Begin extraction process. 
+    extract_contacts(db);
+    printf("\n%s\n","Starting to extract Personal Profile");
+    extract_profile(db);
+
+    sqlite3_close(db); 
+    return EXIT_SUCCESS;
+}
