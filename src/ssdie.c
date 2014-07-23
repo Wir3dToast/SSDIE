50348f9fb435328d866ecc313218413a95511c27
Edward Kim <edbeta12@gmail.com>
2014-07-23 00:03:36 -0400

    Added feature for user to specify which directory to put logs in. Changed some return types.

--- src/ssdie.c
+++ src/ssdie.c
@@ -5,30 +5,43 @@
 
 #include "header/ssdie.h"
 #include "header/sql_functions.h"
+#include "header/file_functions.h"
 
+
+sqlite3* db;
+char* pathname;
 
 int main(int argc, char** argv) {
 
-    char* dbname = argv[1];
   
-    if(argc != 3) {
-      fprintf(stderr, "USAGE: ssdie [DB Path] [DIRNAME]");
+    if(argc != 4) {
+      fprintf(stderr, "USAGE: ssdie [DB Path] [DIRNAME] [DIRPATH]");
       return EXIT_FAILURE;
     }
     
-    sqlite3* db = open_sql_database(dbname);
-    printf("\n%s","Starting to extract Contacts");
+    char* dbname = argv[1];
+    char* rootdir = argv[3]; //Rootdir the directory in which the directory that will contain the result files will be contained in.
+    
+    db = open_sql_database(dbname);
+  
+    pathname = concat_pathname_elements(rootdir,argv[2]); //Create the pathname for the directory that will contain the result files. 
+    check_make_result_dir(pathname); //Create the directory
+
+    printf("\nExtracting to directory: %s",pathname);
+    printf("\n%s","Starting to extract Contacts"); //Begin extraction process. 
     extract_contacts(db);
     printf("\n%s\n","Starting to extract Personal Profile");
     extract_profile(db);
 
+    free(pathname); 
     sqlite3_close(db); 
     return EXIT_SUCCESS;
 }
 
-int extract_profile(sqlite3* db) {
+void extract_profile(sqlite3* db) {
     sqlite3_stmt* profile_stmt = get_sql_statement("select fullname, skypename, city, country, about FROM Accounts;",db);
-    FILE* profilelog= fopen("Profile","w+");
+    char * profile_file_pathname = concat_pathname_elements(pathname,"Profile");
+    FILE* profilelog= fopen(profile_file_pathname,"w+");
     
     fprintf(profilelog, "\n%s\n","PERSONAL PROFILE FOUND");
     
@@ -39,17 +52,19 @@ int extract_profile(sqlite3* db) {
 	fprintf(profilelog,"\nCountry: %s\n", sqlite3_column_text(profile_stmt,3));
  	fprintf(profilelog,"\nAbout: %s\n", sqlite3_column_text(profile_stmt,4));
     }
-
+    
+    //Cleanup and Free memory
+    free(profile_file_pathname);
     free_sql_statement(profile_stmt); 
-    close(profilelog);
-    return EXIT_SUCCESS; 
+    fclose(profilelog); 
 }
 
-int extract_contacts(sqlite3* db) {
+void extract_contacts(sqlite3* db) {
     sqlite3_stmt* contact_stmt = get_sql_statement("select displayname, skypename, phone_mobile, datetime(lastonline_timestamp, 'unixepoch'), about from contacts",db);
-    int result;
-    FILE* contactlog = fopen("Contacts","w+");
-    
+    char* contacts_file_pathname = concat_pathname_elements(pathname,"Contacts");
+    FILE* contactlog = fopen(contacts_file_pathname,"w+");
+    int result;    
+
     fprintf(contactlog,"\n%s\n","PERSONAL CONTACTS FOUND");
     
     do {
@@ -64,9 +79,9 @@ int extract_contacts(sqlite3* db) {
     } while (result == SQLITE_ROW);
    
     //Cleanup and Free memory
+    free(contacts_file_pathname);
     free_sql_statement(contact_stmt);
-    close(contactlog);
-    return EXIT_SUCCESS;  
+    fclose(contactlog); 
 }
 
 void check_ambig_text_column(sqlite3_stmt* stmt, char* columnname, int column, FILE* logfile) {
