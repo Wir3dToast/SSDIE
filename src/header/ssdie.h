e056c44456b84ea4d6ab9473b98fffdc0926ce43
Edward Kim <edbeta12@gmail.com>
2014-07-23 00:02:01 -0400

    Changed return types of extraction functions.

--- src/header/ssdie.h
+++ src/header/ssdie.h
@@ -1,5 +1,5 @@
 //Extractor prototypes
-int extract_contacts(sqlite3*);
-int extract_profile(sqlite3*);
+void extract_contacts(sqlite3*);
+void extract_profile(sqlite3*);
 
 void check_ambig_text_column_(sqlite3_stmt*, char*,int, FILE*);
